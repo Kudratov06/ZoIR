2fa6b4e9e70d9fe1bf29b162b35bec6087ca40b1
Kudratov06 <ibod.kudratov@gmail.com>
2024-10-05 18:49:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title('💩ZoIR first app')
 
 st.write('Hakim vret!')
 
-with st.expander("Initial data"):
+with st.expander('Initial data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   st.wirte("**x**")
   X_raw = df.drop('species', axis = 1 )
