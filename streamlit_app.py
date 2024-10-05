f9b5c77a377cfc72bfc54cfda5a8349136293830
Kudratov06 <ibod.kudratov@gmail.com>
2024-10-05 18:50:57 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ st.write('Hakim vret!')
 
 with st.expander('Initial data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-  st.wirte("**x**")
+  st.wirte('**x**')
   X_raw = df.drop('species', axis = 1 )
   X_raw
 
-  st.wirte("**y**")
+  st.wirte('**y**')
   y_raw = df.species
   y_raw
